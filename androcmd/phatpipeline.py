b20ededbb27700052d820ce9e5530ec8d2622b74
Jonathan Sick <jonathansick@mac.com>
2015-04-22 12:51:02 -0400

    Refactored ColorPlane factories, they take labels

--- androcmd/phatpipeline.py
+++ androcmd/phatpipeline.py
@@ -4,7 +4,6 @@
 Pipelines for fitting PHAT CMDs.
 """
 import os
-from collections import namedtuple, OrderedDict
 from glob import glob
 
 import numpy as np
@@ -18,16 +17,17 @@ from m31hst.phatast import PhatAstTable
 from padova import AgeGridRequest
 from padova.isocdata import join_isochrone_sets, Isochrone
 
-from starfisher import ColorPlane
 from starfisher import Lockfile
 from starfisher import ExtantCrowdingTable
 from starfisher.pipeline import (
-    PipelineBase, IsochroneSetBase, DatasetBase, PlaneBase, LockBase,
+    PipelineBase, IsochroneSetBase, DatasetBase, LockBase,
     CrowdingBase, ExtinctionBase)
+
+from androcmd.planes import RgbPhatPlanes, CompletePhatPlanes
+
 
 PHAT_BANDS = ('F475W', 'F814W', 'F275W', 'F336W', 'F110W', 'F160W')
 STARFISH = os.getenv("STARFISH")
-Lim = namedtuple('Lim', 'x y')
 
 
 class SolarZIsocs(IsochroneSetBase):
@@ -110,7 +110,7 @@ class PhatCatalog(DatasetBase):
         phat_bands = ('F475W', 'F814W', 'F275W', 'F336W', 'F110W', 'F160W')
         # Normalize bandpass names
         for band in phat_bands:
-            old_name = "_".join(band.lower, 'vega')
+            old_name = "_".join((band.lower(), 'vega'))
             self._phat_data.rename_column(old_name, band)
 
     def _make_phot_column(self, band):
@@ -137,40 +137,6 @@ class PhatCatalog(DatasetBase):
         if not os.path.exists(fit_dir):
             os.makedirs(fit_dir)
         np.savetxt(full_path, photdata, delimiter=' ', fmt='%.4f')
-
-
-class CompletePhatPlanes(PlaneBase):
-    """Color planes for PHAT data."""
-    def __init__(self, **kwargs):
-        self._planes = OrderedDict([
-            (('f475w', 'f814w'), make_f475w_f814w()),
-            ('f475w_f814w_rgb', make_f475w_f814w_rgb()),
-            (('f475w', 'f110w'), make_f475w_f110w()),
-            (('f475w', 'f160w'), make_f475w_f160w()),
-            (('f814w', 'f110w'), make_f814w_f110w()),
-            (('f814w', 'f160w'), make_f814w_f160w()),
-            (('f110w', 'f160w'), make_f110w_f160w()),
-        ])
-        print "CompletePhatPlanes", kwargs
-        super(CompletePhatPlanes, self).__init__(**kwargs)
-
-    @property
-    def planes(self):
-        return self._planes
-
-
-class RgbPhatPlanes(PlaneBase):
-    """Color planes for PHAT data."""
-    def __init__(self, **kwargs):
-        self._planes = OrderedDict([
-            ('f475w_f814w_rgb', make_f475w_f814w_rgb()),
-        ])
-        print "RgbPhatPlanes", kwargs
-        super(RgbPhatPlanes, self).__init__(**kwargs)
-
-    @property
-    def planes(self):
-        return self._planes
 
 
 class SolarLockfile(LockBase):
@@ -243,121 +209,3 @@ class SolarRgbPipeline(RgbPhatPlanes, SolarZIsocs, PhatCatalog,
     def __init__(self, **kwargs):
         print "SolarRgbPipeline", kwargs
         super(SolarRgbPipeline, self).__init__(**kwargs)
-
-
-def make_f475w_f814w(dpix=0.05, mag_lim=30.):
-    lim = Lim(x=(-1, 5.), y=(25.5, 20.))
-    plane = ColorPlane((PHAT_BANDS.index('F475W'),
-                        PHAT_BANDS.index('F814W')),
-                       PHAT_BANDS.index('F814W'),
-                       lim.x,
-                       (min(lim.y), max(lim.y)),
-                       mag_lim,
-                       suffix='f475f814',
-                       x_label=r'$\mathrm{F475W}-\mathrm{F814W}$',
-                       y_label=r'$\mathrm{F814W}$',
-                       dpix=dpix)
-    plane.mask_region((3, 5), (28, 25))
-    plane.mask_region((3.5, 5), (25, 23))
-    plane.mask_region((4, 5), (23, 22.5))
-    return plane
-
-
-def make_f475w_f814w_rgb(dpix=0.05, mag_lim=30.):
-    lim = Lim(x=(1.2, 5.), y=(23.5, 20.))
-    plane = ColorPlane((PHAT_BANDS.index('F475W'),
-                        PHAT_BANDS.index('F814W')),
-                       PHAT_BANDS.index('F814W'),
-                       lim.x,
-                       (min(lim.y), max(lim.y)),
-                       mag_lim,
-                       suffix='rgbopt',
-                       x_label=r'$\mathrm{F475W}-\mathrm{F814W}$',
-                       y_label=r'$\mathrm{F814W}$',
-                       dpix=dpix,
-                       nx=75)  # NOTE auto-calc failed to compute 75
-    # plane.mask_region((3, 5), (28, 25))
-    # plane.mask_region((3.5, 5), (25, 23))
-    # plane.mask_region((4, 5), (23, 22.5))
-    return plane
-
-
-def make_f110w_f160w(dpix=0.05, mag_lim=30.):
-    lim = Lim(x=(0.3, 1.3), y=(24., 16.5))
-    plane = ColorPlane((PHAT_BANDS.index('F110W'),
-                        PHAT_BANDS.index('F160W')),
-                       PHAT_BANDS.index('F160W'),
-                       lim.x,
-                       (min(lim.y), max(lim.y)),
-                       mag_lim,
-                       suffix='f110f160',
-                       x_label=r'$\mathrm{F110W}-\mathrm{F160W}$',
-                       y_label=r'$\mathrm{F160W}$',
-                       dpix=dpix)
-    plane.mask_region((-1., 0.), (22., 16))
-    plane.mask_region((0, 0.3), (22., 16))
-    plane.mask_region((0.3, 0.7), (20., 16))
-    plane.mask_region((0.7, 0.8), (19., 16))
-    plane.mask_region((0.8, 0.9), (18., 16))
-    plane.mask_region((1.1, 1.5), (28, 21))
-    return plane
-
-
-def make_f475w_f160w(dpix=0.05, mag_lim=30.):
-    lim = Lim(x=(-0.8, 8.), y=(25., 17.5))
-    plane = ColorPlane((PHAT_BANDS.index('F475W'),
-                        PHAT_BANDS.index('F160W')),
-                       PHAT_BANDS.index('F160W'),
-                       lim.x,
-                       (min(lim.y), max(lim.y)),
-                       mag_lim,
-                       suffix='f475f160',
-                       x_label=r'$\mathrm{F475W}-\mathrm{F160W}$',
-                       y_label=r'$\mathrm{F110W}$',
-                       dpix=dpix)
-    return plane
-
-
-def make_f475w_f110w(dpix=0.05, mag_lim=30.):
-    lim = Lim(x=(-0.8, 7.), y=(25., 18.))
-    plane = ColorPlane((PHAT_BANDS.index('F475W'),
-                        PHAT_BANDS.index('F110W')),
-                       PHAT_BANDS.index('F110W'),
-                       lim.x,
-                       (min(lim.y), max(lim.y)),
-                       mag_lim,
-                       suffix='f475f110',
-                       x_label=r'$\mathrm{F475W}-\mathrm{F110W}$',
-                       y_label=r'$\mathrm{F110W}$',
-                       dpix=dpix)
-    return plane
-
-
-def make_f814w_f110w(dpix=0.05, mag_lim=30.):
-    lim = Lim(x=(-0.1, 1.8), y=(25, 19))
-    plane = ColorPlane((PHAT_BANDS.index('F814W'),
-                        PHAT_BANDS.index('F110W')),
-                       PHAT_BANDS.index('F110W'),
-                       lim.x,
-                       (min(lim.y), max(lim.y)),
-                       mag_lim,
-                       suffix='f814f110',
-                       x_label=r'$\mathrm{F814W}-\mathrm{F110W}$',
-                       y_label=r'$\mathrm{F110W}$',
-                       dpix=dpix)
-    return plane
-
-
-def make_f814w_f160w(dpix=0.05, mag_lim=30.):
-    lim = Lim(x=(-0.5, 3), y=(24, 17.5))
-    plane = ColorPlane((PHAT_BANDS.index('F814W'),
-                        PHAT_BANDS.index('F160W')),
-                       PHAT_BANDS.index('F160W'),
-                       lim.x,
-                       (min(lim.y), max(lim.y)),
-                       mag_lim,
-                       suffix='f814f160',
-                       x_label=r'$\mathrm{F814W}-\mathrm{F160W}$',
-                       y_label=r'$\mathrm{F160W}$',
-                       dpix=dpix)
-    return plane
---
+++ androcmd/planes.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Factories for ColorPlanes used in modelling.
+
+2015-04-22 - Created by Jonathan Sick
+"""
+
+from collections import namedtuple, OrderedDict
+from starfisher import ColorPlane
+from starfisher.pipeline import PlaneBase
+
+
+Lim = namedtuple('Lim', 'x y')
+
+
+class CompletePhatPlanes(PlaneBase):
+    """Color planes for PHAT data."""
+    def __init__(self, **kwargs):
+        self._planes = OrderedDict([
+            (('f475w', 'f814w'), make_f475w_f814w()),
+            ('f475w_f814w_rgb', make_f475w_f814w_rgb()),
+            (('f475w', 'f110w'), make_f475w_f110w()),
+            (('f475w', 'f160w'), make_f475w_f160w()),
+            (('f814w', 'f110w'), make_f814w_f110w()),
+            (('f814w', 'f160w'), make_f814w_f160w()),
+            (('f110w', 'f160w'), make_f110w_f160w()),
+        ])
+        print "CompletePhatPlanes", kwargs
+        super(CompletePhatPlanes, self).__init__(**kwargs)
+
+    @property
+    def planes(self):
+        return self._planes
+
+
+class RgbPhatPlanes(PlaneBase):
+    """Color planes for PHAT data."""
+    def __init__(self, **kwargs):
+        self._planes = OrderedDict([
+            # ('f475w_f814w_rgb', make_f475w_f814w_rgb_hack()),
+            ('f475w_f814w_rgb', make_f475w_f814w_rgb()),
+        ])
+        print "RgbPhatPlanes", kwargs
+        super(RgbPhatPlanes, self).__init__(**kwargs)
+
+    @property
+    def planes(self):
+        return self._planes
+
+
+def make_f475w_f814w(dpix=0.05, mag_lim=30.):
+    lim = Lim(x=(-1, 5.), y=(25.5, 20.))
+    plane = ColorPlane(('F475W', 'F814W'), 'F814W',
+                       lim.x,
+                       (min(lim.y), max(lim.y)),
+                       mag_lim,
+                       suffix='f475f814',
+                       x_label=r'$\mathrm{F475W}-\mathrm{F814W}$',
+                       y_label=r'$\mathrm{F814W}$',
+                       dpix=dpix)
+    plane.mask_region((3, 5), (28, 25))
+    plane.mask_region((3.5, 5), (25, 23))
+    plane.mask_region((4, 5), (23, 22.5))
+    return plane
+
+
+def make_f475w_f814w_rgb_hack(dpix=0.05, mag_lim=30.):
+    lim = Lim(x=(1.2, 5.), y=(23.5, 20.))
+    plane = ColorPlane(('F475W', 'F814W'), 'F814W',
+                       lim.x,
+                       (min(lim.y), max(lim.y)),
+                       mag_lim,
+                       suffix='rgbopt',
+                       x_label=r'$\mathrm{F475W}-\mathrm{F814W}$',
+                       y_label=r'$\mathrm{F814W}$',
+                       dpix=dpix,
+                       nx=75)  # NOTE auto-calc failed to compute 75
+    # plane.mask_region((3, 5), (28, 25))
+    # plane.mask_region((3.5, 5), (25, 23))
+    # plane.mask_region((4, 5), (23, 22.5))
+    return plane
+
+
+def make_f475w_f814w_rgb(dpix=0.05, mag_lim=30.):
+    lim = Lim(x=(1.2, 5.), y=(23.5, 20.))
+    plane = ColorPlane(('F475W', 'F814W'), 'F814W',
+                       lim.x,
+                       (min(lim.y), max(lim.y)),
+                       mag_lim,
+                       suffix='rgbopt',
+                       x_label=r'$\mathrm{F475W}-\mathrm{F814W}$',
+                       y_label=r'$\mathrm{F814W}$',
+                       dpix=dpix)
+    # plane.mask_region((3, 5), (28, 25))
+    # plane.mask_region((3.5, 5), (25, 23))
+    # plane.mask_region((4, 5), (23, 22.5))
+    return plane
+
+
+def make_f110w_f160w(dpix=0.05, mag_lim=30.):
+    lim = Lim(x=(0.3, 1.3), y=(24., 16.5))
+    plane = ColorPlane(('F110W', 'F160W'), 'F160W',
+                       lim.x,
+                       (min(lim.y), max(lim.y)),
+                       mag_lim,
+                       suffix='f110f160',
+                       x_label=r'$\mathrm{F110W}-\mathrm{F160W}$',
+                       y_label=r'$\mathrm{F160W}$',
+                       dpix=dpix)
+    plane.mask_region((-1., 0.), (22., 16))
+    plane.mask_region((0, 0.3), (22., 16))
+    plane.mask_region((0.3, 0.7), (20., 16))
+    plane.mask_region((0.7, 0.8), (19., 16))
+    plane.mask_region((0.8, 0.9), (18., 16))
+    plane.mask_region((1.1, 1.5), (28, 21))
+    return plane
+
+
+def make_f475w_f160w(dpix=0.05, mag_lim=30.):
+    lim = Lim(x=(-0.8, 8.), y=(25., 17.5))
+    plane = ColorPlane(('F475W', 'F160W'), 'F160W',
+                       lim.x,
+                       (min(lim.y), max(lim.y)),
+                       mag_lim,
+                       suffix='f475f160',
+                       x_label=r'$\mathrm{F475W}-\mathrm{F160W}$',
+                       y_label=r'$\mathrm{F110W}$',
+                       dpix=dpix)
+    return plane
+
+
+def make_f475w_f110w(dpix=0.05, mag_lim=30.):
+    lim = Lim(x=(-0.8, 7.), y=(25., 18.))
+    plane = ColorPlane(('F475W', 'F110W'), 'F110W',
+                       lim.x,
+                       (min(lim.y), max(lim.y)),
+                       mag_lim,
+                       suffix='f475f110',
+                       x_label=r'$\mathrm{F475W}-\mathrm{F110W}$',
+                       y_label=r'$\mathrm{F110W}$',
+                       dpix=dpix)
+    return plane
+
+
+def make_f814w_f110w(dpix=0.05, mag_lim=30.):
+    lim = Lim(x=(-0.1, 1.8), y=(25, 19))
+    plane = ColorPlane(('F814W', 'F110W'), 'F110W',
+                       lim.x,
+                       (min(lim.y), max(lim.y)),
+                       mag_lim,
+                       suffix='f814f110',
+                       x_label=r'$\mathrm{F814W}-\mathrm{F110W}$',
+                       y_label=r'$\mathrm{F110W}$',
+                       dpix=dpix)
+    return plane
+
+
+def make_f814w_f160w(dpix=0.05, mag_lim=30.):
+    lim = Lim(x=(-0.5, 3), y=(24, 17.5))
+    plane = ColorPlane(('F814W', 'F160W'), 'F160W',
+                       lim.x,
+                       (min(lim.y), max(lim.y)),
+                       mag_lim,
+                       suffix='f814f160',
+                       x_label=r'$\mathrm{F814W}-\mathrm{F160W}$',
+                       y_label=r'$\mathrm{F160W}$',
+                       dpix=dpix)
+    return plane
