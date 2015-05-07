df084c5b3cd8b23d00d3e08033b06e7a95b77147
Jonathan Sick <jonathansick@mac.com>
2015-05-06 23:41:40 -0400

    Add fit plot to three_z.py

--- scripts/three_z.py
+++ scripts/three_z.py
@@ -29,12 +29,16 @@ def main():
         plane = fit_planes[fit_name]
         pipeline.fit(fit_name, plane, dataset)
 
+    if args.plot:
+        plot_fit(pipeline, dataset)
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Multi-metallicity fitting")
     parser.add_argument('brick', type=int)
     parser.add_argument('--fit', nargs='*', default=list())
+    parser.add_argument('--plot', default=False, action='store_true')
     return parser.parse_args()
 
 
@@ -45,6 +49,98 @@ class Pipeline(BasicPhatPlanes,
     def __init__(self, **kwargs):
         print "MultiZPhatPipeline", kwargs
         super(Pipeline, self).__init__(**kwargs)
+
+
+def plot_fit(pipeline, dataset):
+    """Plot for the thesis"""
+    fit_key = 'f475w_f160w'
+    plane_key = 'f475w_f160w'
+    plotpath = "b23_three_z_f475w_f160w"
+
+    import numpy as np
+    from palettable.cubehelix import perceptual_rainbow_16
+    from palettable.colorbrewer.diverging import RdBu_11
+    import matplotlib as mpl
+    from matplotlib.figure import Figure
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    import matplotlib.gridspec as gridspec
+
+    cube_map = perceptual_rainbow_16.mpl_colormap
+
+    obs_hess = pipeline.make_obs_hess(dataset, plane_key)
+    fit_hess = pipeline.make_fit_hess(fit_key, plane_key)
+    sigma = np.sqrt(obs_hess.hess)
+    chi = ((obs_hess.hess - fit_hess.hess) / sigma) ** 2.
+    diff = obs_hess.hess - fit_hess.hess
+
+    fig = Figure(figsize=(7, 3.5), frameon=False)
+    canvas = FigureCanvas(fig)
+    gs = gridspec.GridSpec(2, 4, wspace=0.15, hspace=0.2,
+                           left=0.08, bottom=0.15, right=0.95,
+                           width_ratios=(1, 1, 1, 1), height_ratios=(0.1, 1))
+    ax_obs = fig.add_subplot(gs[1, 0])
+    ax_model = fig.add_subplot(gs[1, 1])
+    ax_chi = fig.add_subplot(gs[1, 2])
+    ax_diff = fig.add_subplot(gs[1, 3])
+    ax_obs_cb = fig.add_subplot(gs[0, 0])
+    ax_model_cb = fig.add_subplot(gs[0, 1])
+    ax_chi_cb = fig.add_subplot(gs[0, 2])
+    ax_diff_cb = fig.add_subplot(gs[0, 3])
+
+    fit_map = pipeline.plot_fit_hess(ax_model, fit_key, plane_key,
+                                     imshow=dict(vmin=0, vmax=3.,
+                                                 cmap=cube_map))
+    ax_model.yaxis.set_major_formatter(mpl.ticker.NullFormatter())
+    ax_model.set_ylabel('')
+    fit_cb = fig.colorbar(fit_map, cax=ax_model_cb,
+                          orientation='horizontal')
+    fit_cb.set_label(r"$\log(N_*)$ Model", size=9)
+    fit_cb.ax.xaxis.set_ticks_position('top')
+    fit_cb.locator = mpl.ticker.MultipleLocator(1.0)
+    for tl in fit_cb.ax.get_xmajorticklabels():
+        tl.set_size(8.)
+    fit_cb.update_ticks()
+
+    obs_map = pipeline.plot_obs_hess(ax_obs, dataset, plane_key,
+                                     imshow=dict(vmin=0, vmax=3.,
+                                                 cmap=cube_map))
+    ax_obs.yaxis.set_major_formatter(mpl.ticker.NullFormatter())
+    obs_cb = fig.colorbar(obs_map, cax=ax_obs_cb, orientation='horizontal')
+    obs_cb.set_label(r"$\log(N_*)$ Obs.", size=9)
+    obs_cb.ax.xaxis.set_ticks_position('top')
+    obs_cb.locator = mpl.ticker.MultipleLocator(1.0)
+    for tl in obs_cb.ax.get_xmajorticklabels():
+        tl.set_size(8.)
+    obs_cb.update_ticks()
+
+    chi_map = pipeline.plot_hess_array(ax_chi, chi, plane_key, log=False,
+                                       imshow=dict(vmax=20, cmap=cube_map))
+    ax_chi.yaxis.set_major_formatter(mpl.ticker.NullFormatter())
+    ax_chi.set_ylabel('')
+    chi_cb = fig.colorbar(chi_map, cax=ax_chi_cb, orientation='horizontal')
+    chi_cb.set_label(r"$\chi^2$", size=9)
+    chi_cb.ax.xaxis.set_ticks_position('top')
+    chi_cb.locator = mpl.ticker.MultipleLocator(5)
+    for tl in chi_cb.ax.get_xmajorticklabels():
+        tl.set_size(8.)
+    chi_cb.update_ticks()
+
+    diff_map = pipeline.plot_hess_array(ax_diff, diff, plane_key, log=False,
+                                        imshow=dict(vmin=-50, vmax=50,
+                                                    cmap=RdBu_11.mpl_colormap))
+    ax_diff.yaxis.set_major_formatter(mpl.ticker.NullFormatter())
+    ax_diff.set_ylabel('')
+    diff_cb = fig.colorbar(diff_map, cax=ax_diff_cb,
+                           orientation='horizontal')
+    diff_cb.set_label(r"$\Delta_\mathrm{obs-model}$ ($N_*$)", size=9)
+    diff_cb.ax.xaxis.set_ticks_position('top')
+    diff_cb.locator = mpl.ticker.MultipleLocator(20)
+    for tl in diff_cb.ax.get_xmajorticklabels():
+        tl.set_size(8.)
+    diff_cb.update_ticks()
+
+    gs.tight_layout(fig, pad=1.08, h_pad=None, w_pad=None, rect=None)
+    canvas.print_figure(plotpath + ".pdf", format="pdf")
 
 
 if __name__ == '__main__':
