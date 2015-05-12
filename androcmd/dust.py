6d06bcd0bccf5bc0e651adecea09f7fae99ba436
Jonathan Sick <jonathansick@mac.com>
2015-05-12 18:07:05 -0400

    Add LewisDustLaw

--- androcmd/dust.py
+++ androcmd/dust.py
@@ -7,7 +7,9 @@ Tools for working with dust attenuation for M31.
 """
 
 import numpy as np
+import astropy
 
+from m31hst.draine import spire350_dust_mass_map
 from starfisher.dust import SF11ExtinctionCurve
 
 
@@ -29,3 +31,23 @@ def phat_rel_extinction():
     rel_extinction[4] = curve.extinction_ratio('WFC3 F110W')
     rel_extinction[5] = curve.extinction_ratio('WFC3 F160W')
     return rel_extinction
+
+
+class LewisDustLaw(object):
+    """Assign extinction Av+dAv from the Drain et al dust map."""
+    def __init__(self):
+        super(LewisDustLaw, self).__init__()
+        self._f = astropy.io.fits.open(spire350_dust_mass_map())
+        self._wcs = astropy.wcs.WCS(self._f[0].header)
+        self._naxis1 = self._f[0].header['NAXIS1']
+        self._naxis2 = self._f[0].header['NAXIS2']
+
+    def estimate_extinction(self, ra, dec):
+        """Estimate the maximum Av estinction to this coordinate."""
+        x, y = self.wcs.all_world2pix([ra], [dec], 0)
+        assert x > 0 and x < self._naxis1
+        assert y > 0 and y < self._naxis2
+        x = int(x)
+        y = int(y)
+        Sigma_dust = self._f[0].data[y, x]
+        return 10. ** (-5.4) * Sigma_dust
