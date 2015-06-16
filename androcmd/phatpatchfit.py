66a5632b4491b71475731424368dc4ded06d5ac9
Jonathan Sick <jonathansick@mac.com>
2015-06-16 16:29:03 -0400

    Copy galaxy_coords function into codebase

--- androcmd/phatpatchfit.py
+++ androcmd/phatpatchfit.py
@@ -322,7 +322,6 @@ def compute_patch_gal_coords(ra, dec):
     """Compute the galactocentric coordinates (radius in kpc; PA in degrees)
     for a patch given its central coordinate.
     """
-    from wedge.galaxycoords import galaxy_coords
     d0 = Distance(785, unit=u.kpc)
     pa0 = Angle('37d42m54s')
     incl0 = Angle('77.5d')
@@ -330,3 +329,56 @@ def compute_patch_gal_coords(ra, dec):
     coord = SkyCoord(ra * u.degree, dec * u.degree)
     r, phi, r_sky = galaxy_coords(coord, coord0, pa0, incl0, d0)
     return r.kpc, phi.degree
+
+
+def galaxy_coords(coord, glx_ctr, glx_PA, glx_incl, glx_dist):
+    """Computes deprojected galactocentric distance.
+    Inspired by: http://idl-moustakas.googlecode.com/svn-history/
+        r560/trunk/impro/hiiregions/im_hiiregion_deproject.pro
+    Parameters
+    ----------
+    coord : :class:`astropy.coordinates.SkyCoord`
+        Coordinate of points to compute galactocentric distance for.
+        Can be either a single coordinate, or array of coordinates.
+    glx_ctr : :class:`astropy.coordinates.SkyCoord`
+        Galaxy center.
+    glx_PA : :class:`astropy.coordinates.Angle`
+        Position angle of galaxy disk.
+    glx_incl : :class:`astropy.coordinates.Angle`
+        Inclination angle of the galaxy disk.
+    glx_dist : :class:`astropy.coordinates.Distance`
+        Distance to galaxy.
+    Returns
+    -------
+    obj_dist : class:`astropy.coordinates.Distance`
+        Galactocentric distance(s) for coordinate point(s).
+    """
+    # distance from coord to glx centre
+    sky_radius = glx_ctr.separation(coord)
+    avg_dec = 0.5 * (glx_ctr.dec + coord.dec).radian
+    x = (glx_ctr.ra - coord.ra) * np.cos(avg_dec)
+    y = glx_ctr.dec - coord.dec
+    # azimuthal angle from coord to glx  -- not completely happy with this
+    phi = glx_PA - Angle('90d') \
+        + Angle(np.arctan2(y.arcsec, x.arcsec), unit=u.rad)
+
+    # convert to coordinates in rotated frame, where y-axis is galaxy major
+    # ax; have to convert to arcmin b/c can't do sqrt(x^2+y^2) when x and y
+    # are angles
+    xp = (sky_radius * np.cos(phi.radian)).arcmin
+    yp = (sky_radius * np.sin(phi.radian)).arcmin
+
+    # de-project
+    ypp = yp / np.cos(glx_incl.radian)
+    obj_radius = np.sqrt(xp ** 2 + ypp ** 2)  # in arcmin
+    obj_dist = Distance(Angle(obj_radius, unit=u.arcmin).radian * glx_dist,
+                        unit=glx_dist.unit)
+
+    # Computing PA in disk
+    # negative sign needed to get correct orientation from major axis
+    obj_phi = Angle(np.arctan2(ypp, -xp), unit=u.rad)
+    # FIXME only for scalars
+    if obj_phi < 0.:
+        obj_phi = Angle(2. * np.pi, unit=u.rad) + obj_phi
+
+    return obj_dist, obj_phi, sky_radius.arcsec
