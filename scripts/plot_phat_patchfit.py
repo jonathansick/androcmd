8f5f01fd96b739721b731cab5648821900c8733c
Jonathan Sick <jonathansick@mac.com>
2015-06-23 12:42:14 -0400

    Plot SFH of each patch, keyed by radius

--- scripts/plot_phat_patchfit.py
+++ scripts/plot_phat_patchfit.py
@@ -10,11 +10,13 @@ Use the fetch_patch_results.py script to build a patch dataset.
 
 import argparse
 import h5py
+import numpy as np
 
-# import matplotlib as mpl
+import matplotlib as mpl
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.gridspec as gridspec
+from palettable.cubehelix import perceptual_rainbow_16
 
 
 def main():
@@ -31,6 +33,9 @@ def main():
     if args.rchi_hist is not None:
         plot_reduced_chi_hist(dataset, args.rchi_hist)
 
+    if args.sfh_lines is not None:
+        plot_sfh_lines(dataset, args.sfh_lines)
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -38,6 +43,7 @@ def parse_args():
     parser.add_argument('--radial-sfh-points', default=None)
     parser.add_argument('--radial-sfh-intervals', default=None)
     parser.add_argument('--rchi-hist', default=None)
+    parser.add_argument('--sfh-lines', default=None)
     return parser.parse_args()
 
 
@@ -110,6 +116,65 @@ def plot_reduced_chi_hist(dataset, plot_path):
     ax.legend()
     gs.tight_layout(fig, pad=1.08, h_pad=None, w_pad=None, rect=None)
     canvas.print_figure(plot_path + ".pdf", format="pdf")
+
+
+def plot_sfh_lines(dataset, plot_path):
+    fig = Figure(figsize=(6, 6), frameon=False)
+    canvas = FigureCanvas(fig)
+    gs = gridspec.GridSpec(2, 2,
+                           left=0.1, right=0.9, bottom=0.1, top=0.95,
+                           wspace=0.1, hspace=0.1,
+                           width_ratios=(1, 0.1), height_ratios=None)
+    ax_ms = fig.add_subplot(gs[0, 0])
+    ax_oir = fig.add_subplot(gs[1, 0])
+    ax_cb = fig.add_subplot(gs[:, 1])
+
+    patches = dataset['patches']
+    cmap = perceptual_rainbow_16.mpl_colormap
+    radius_normalizer = mpl.colors.Normalize(vmin=0, vmax=25, clip=True)
+    r_mapper = mpl.cm.ScalarMappable(norm=radius_normalizer, cmap=cmap)
+    radii = []
+    for patch_name, patch_group in patches.items():
+        for fit_key, ax in zip(('lewis', 'oir_all'), (ax_ms, ax_oir)):
+            r_kpc = patch_group.attrs['r_kpc']
+            radii.append(r_kpc)
+            sfh_table = patch_group['sfh'][fit_key]
+            logage, sfr = marginalize_metallicity(sfh_table)
+            ax.plot(logage, np.log10(sfr), '-', lw=0.5,
+                    c=r_mapper.to_rgba(r_kpc, alpha=0.5))
+    r_mapper.set_array(np.array(radii))
+    cbar = fig.colorbar(r_mapper, cax=ax_cb, orientation='vertical')
+    cbar.set_label(r'$R_\mathrm{maj}$')
+    for ax in (ax_ms, ax_oir):
+        ax.set_xlim(6.4, 10.2)
+        ax.set_ylabel(r'$\log(\mathrm{SFR})$')
+        ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(base=1))
+    for tl in ax_ms.get_xmajorticklabels():
+        tl.set_visible(False)
+    ax_oir.set_xlabel(r'$\log(A~\mathrm{Gyr}^{-1})$')
+    gs.tight_layout(fig, pad=1.08, h_pad=None, w_pad=None, rect=None)
+    canvas.print_figure(plot_path + ".pdf", format="pdf")
+
+
+def marginalize_metallicity(sfh_table):
+    t = np.empty(len(sfh_table), dtype=sfh_table.dtype)
+    sfh_table.read_direct(t, source_sel=None, dest_sel=None)
+    age_vals = np.unique(t['log(age)'])
+    s = np.argsort(age_vals)
+    age_vals = age_vals[s]
+    A = []
+    sfr = []
+    for i, age_val in enumerate(age_vals):
+        tt = t[t['log(age)'] == age_val]
+        bin_sfr = np.sum(tt['sfr'])
+        A.append(age_val)
+        sfr.append(bin_sfr)
+    srt = np.argsort(A)
+    A = np.array(A)
+    sfr = np.array(sfr)
+    A = A[srt]
+    sfr = sfr[srt]
+    return A, sfr
 
 
 if __name__ == '__main__':
