92ed6b26525efd8f6d88dfce040b6762a034b8fd
Jonathan Sick <jonathansick@mac.com>
2015-07-17 14:01:20 -0400

    plot_estimates_grid plot for magphys in phat fields

--- scripts/starfish_magphys/plot_magphys_phat_patches.py
+++ scripts/starfish_magphys/plot_magphys_phat_patches.py
@@ -9,6 +9,7 @@ Plot MAGPHYS estimates of PHAT fields.
 import argparse
 import os
 
+import numpy as np
 import h5py
 import matplotlib as mpl
 from matplotlib.figure import Figure
@@ -24,7 +25,8 @@ def main():
     args = parse_args()
     base_path = os.path.splitext(args.path)[0]
 
-    plot_mean_age(args.path, base_path + '_mean_age')
+    # plot_mean_age(args.path, base_path + '_mean_age')
+    plot_estimates_grid(args.path, base_path + '_est_grid')
 
 
 def parse_args():
@@ -75,10 +77,110 @@ def plot_mean_age(sed_path, plot_path):
     canvas.print_figure(plot_path + ".pdf", format="pdf")
 
 
+def plot_estimates_grid(sed_path, plot_path):
+    dataset = h5py.File(sed_path, 'r')
+
+    fig = Figure(figsize=(6.5, 7), frameon=False)
+    canvas = FigureCanvas(fig)
+    gs = gridspec.GridSpec(2, 3,
+                           left=0.04, right=0.96, bottom=0.07, top=0.95,
+                           wspace=0.15, hspace=0.15,
+                           width_ratios=None, height_ratios=None)
+    gs_settings = dict(height_ratios=(1, 0.05), hspace=0.01)
+    gs_age = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=gs[0, 0],
+                                              **gs_settings)
+    gs_sfr = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=gs[1, 0],
+                                              **gs_settings)
+    gs_Z = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=gs[0, 1],
+                                            **gs_settings)
+    gs_mu = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=gs[1, 1],
+                                             **gs_settings)
+    gs_tau = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=gs[0, 2],
+                                              **gs_settings)
+    gs_ism = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=gs[1, 2],
+                                              **gs_settings)
+
+    t = dataset['estimates']
+    _plot_grid_ax(10. ** (t['log_age_M'] - 9.), fig, gs_age,
+                  vmin=0., vmax=12.,
+                  label=r'$\langle A~\mathrm{Gyr}^{-1} \rangle$')
+
+    _plot_grid_ax(t['Z_Zo'], fig, gs_Z,
+                  vmin=-2, vmax=0.3,
+                  label=r'$\log Z/Z_\odot$')
+
+    print t.dtype
+    _plot_grid_ax(scale_sfr(t['SFR_0.1Gyr']), fig, gs_sfr,
+                  vmin=1.0, vmax=1.25,
+                  label=r'$\langle \log \mathrm{SFR}_{100~\mathrm{Myr}} \rangle$')
+
+    _plot_grid_ax(t['mu'], fig, gs_mu,
+                  vmin=0., vmax=1,
+                  label=r'$\mu$')
+
+    _plot_grid_ax(t['tau_V'], fig, gs_tau,
+                  vmin=0., vmax=2,
+                  label=r'$\tau_V$')
+
+    _plot_grid_ax(t['tau_V_ISM'], fig, gs_ism,
+                  vmin=0., vmax=0.5,
+                  label=r'$\tau_\mathrm{ISM}$')
+
+    canvas.print_figure(plot_path + ".pdf", format="pdf")
+
+
+def _plot_grid_ax(data, fig, gs, vmin=0., vmax=1.,
+                  label=None, cb_locator=mpl.ticker.MaxNLocator(4)):
+    basemap = load_galex_map()
+    polys = _make_footprint_list()
+
+    ax = setup_galex_axes(fig, gs[0], basemap)
+    ax_cb = fig.add_subplot(gs[1])
+    print data[:, 2].flatten()
+    cmap = perceptual_rainbow_16.mpl_colormap
+    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax, clip=True)
+    mapper = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+    mapper.set_array(data[:, 2].flatten())
+
+    poly_args = {'edgecolor': 'k', 'lw': 0.5,
+                 'transform': ax.get_transform('world'),
+                 'closed': True}
+    for poly, v in zip(polys, data):
+        patch = mpl.patches.Polygon(poly,
+                                    facecolor=mapper.to_rgba(v[2]),
+                                    **poly_args)
+        ax.add_patch(patch)
+    cb = fig.colorbar(mapper, cax=ax_cb, orientation='horizontal')
+    if label is not None:
+        cb.set_label(label, fontsize=9)
+        cb.ax.tick_params(labelsize=7)
+        cb.locator = cb_locator
+        cb.update_ticks()
+
+    ax.coords[0].ticklabels.set_visible(False)
+    ax.coords[1].ticklabels.set_visible(False)
+
+
 def _make_footprint_list():
     """Make a list of footprints that matches the SED ID order"""
     fields = load_field_patches()
     return [patch['poly'] for patch in fields]
+
+
+def scale_sfr(sfr):
+    fields = load_field_patches()
+    area_proj = np.atleast_2d(np.array([f['area_proj'] for f in fields])).T
+    print "area_proj"
+    print area_proj
+    area = area_proj / np.cos(77.5 * np.pi / 180.) / 1e3 / 1e3  # kpc^2
+    print "area"
+    print area
+    print "sfr"
+    print sfr
+    scaled_sfr = np.log10(10. ** sfr / area * 10. ** 3.)
+    print "scaled log(sfr)"
+    print scaled_sfr
+    return scaled_sfr
 
 
 if __name__ == '__main__':
