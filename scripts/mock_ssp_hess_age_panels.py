f6cb4a474e3c4f9011a64e4c62025e167857c783
Jonathan Sick <jsick@lsst.org>
2015-12-14 10:23:56 -0700

    Add script showing fits to each age bin of mock SSPs

--- scripts/mock_ssp_hess_age_panels.py
+++ scripts/mock_ssp_hess_age_panels.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
 """
-Plot mock and residual OIR-ALL hess diagrams for all AST fields, but at a
-single age.
+Plot the mock and residual Hess diagrams for a single plane for a single
+AST field, but all ages.
 
-2015-12-12 - Created by Jonathan Sick
+2015-12-14 - Created by Jonathan Sick
 """
 
 import os
@@ -25,8 +27,11 @@ HessPlane = namedtuple('HessPlane',
 
 
 def main():
-    plot('mock_ssp_hess_residuals_5370myr', 'ssp_5370myr_solar')
-    plot('mock_ssp_hess_residuals_1380myr', 'ssp_1380myr_solar')
+    args = [(6, 'oir_all')]
+    for ast_field, plane_name in args:
+        plot('mock_ssp_hess_age_grid_{0:d}_{plane}'.format(ast_field,
+                                                           plane_name),
+             ast_field, plane_name)
 
 
 def load_dataset(ast_number, model_name):
@@ -36,6 +41,7 @@ def load_dataset(ast_number, model_name):
     f = h5py.File(path, 'r')
 
     mock = f['mocksfh'][model_name]['obs_hess']['oir_all']
+    print mock.attrs.keys()
     mock_image = np.log10(mock)
     mock_image = np.ma.masked_invalid(mock_image)
     mock_hess = HessPlane(image=mock_image,
@@ -59,17 +65,20 @@ def load_dataset(ast_number, model_name):
     return mock_hess, diff_hess
 
 
-def plot(plot_path, model_name):
-    ast_fields = [6, 5, 4, 3]
+def plot(plot_path, ast_field, plane_name):
+    ages = (53.7, 100.0, 186.2, 346.7, 645.7, 1380.4, 3467.4, 5370.3, 9549.9)
+    model_names = ['ssp_{0:d}myr_solar'.format(int(myr)) for myr in ages]
+    n_models = len(model_names)
+
     d_xticks = 2.
     d_yticks = 1.
 
     fig = Figure(figsize=(6.5, 4.0), frameon=False)
     canvas = FigureCanvas(fig)
-    gs = gridspec.GridSpec(2, len(ast_fields) + 1,
+    gs = gridspec.GridSpec(2, n_models + 1,
                            left=0.08, right=0.9, bottom=0.15, top=0.90,
                            wspace=0.15, hspace=0.15,
-                           width_ratios=[1. for i in ast_fields] + [0.1],
+                           width_ratios=[1. for i in model_names] + [0.1],
                            height_ratios=None)
 
     ax_mock_cb = fig.add_subplot(gs[0, -1])
@@ -77,14 +86,14 @@ def plot(plot_path, model_name):
     axes_mock = []
     axes_diff = []
 
-    for i, ast_field in enumerate(ast_fields):
+    for i, (model_name, age) in enumerate(zip(ages, model_names)):
         ax_mock = fig.add_subplot(gs[0, i])
         axes_mock.append(ax_mock)
         ax_diff = fig.add_subplot(gs[1, i])
         axes_diff.append(ax_diff)
 
         ax_mock.text(0.5, 1.04,
-                     r'AST \#{0:d}'.format(ast_field),
+                     r'$A = {0:.0f}$~Myr'.format(age),
                      va='baseline', ha='center',
                      transform=ax_mock.transAxes)
 
@@ -109,7 +118,7 @@ def plot(plot_path, model_name):
             mpl.ticker.MultipleLocator(base=d_xticks))
         ax_mock.yaxis.set_major_locator(
             mpl.ticker.MultipleLocator(base=d_yticks))
-        if i == len(ast_fields) - 1:
+        if i == n_models - 1:
             cb = fig.colorbar(im, cax=ax_mock_cb, orientation='vertical')
             cb.set_label(r"$\log(N_*)$ Mock")
             # cb.ax.xaxis.set_ticks_position('top')
@@ -134,7 +143,7 @@ def plot(plot_path, model_name):
             mpl.ticker.MultipleLocator(base=d_xticks))
         ax_diff.yaxis.set_major_locator(
             mpl.ticker.MultipleLocator(base=d_yticks))
-        if i == len(ast_fields) - 1:
+        if i == n_models - 1:
             cb = fig.colorbar(im, cax=ax_diff_cb, orientation='vertical')
             cb.set_label(r"$\Delta_\mathrm{mock-model}$ ($N_*$)")
             cb.update_ticks()
